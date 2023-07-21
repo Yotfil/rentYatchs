dc7f16c731ee705cad0db08144c67eb4d33abac3
Jefry Guevara <jefryeguevarav@gmail.com>
2023-07-21 18:48:30 -0500

    about us added and some new products

--- src/data/absolute_60ft.js
+++ src/data/absolute_60ft.js
@@ -1,12 +1,12 @@
-import img1 from '../assets/Absolute 60ft/1.jpg'
-import img2 from '../assets/Absolute 60ft/2.jpg'
-import img3 from '../assets/Absolute 60ft/3.jpg'
-import img4 from '../assets/Absolute 60ft/4.jpg'
-import img5 from '../assets/Absolute 60ft/5.jpg'
-import img6 from '../assets/Absolute 60ft/6.jpg'
-import img7 from '../assets/Absolute 60ft/7.jpg'
-import img8 from '../assets/Absolute 60ft/8.jpg'
-import img9 from '../assets/Absolute 60ft/9.jpg'
+import img1 from '../assets/Absolute60ft/1.jpg'
+import img2 from '../assets/Absolute60ft/2.jpg'
+import img3 from '../assets/Absolute60ft/3.jpg'
+import img4 from '../assets/Absolute60ft/4.jpg'
+import img5 from '../assets/Absolute60ft/5.jpg'
+import img6 from '../assets/Absolute60ft/6.jpg'
+import img7 from '../assets/Absolute60ft/7.jpg'
+import img8 from '../assets/Absolute60ft/8.jpg'
+import img9 from '../assets/Absolute60ft/9.jpg'
 
 export const imagesAbsolute60ft = {
   img1,
--- src/data/sea_ray_sundancer38ft.js
+++ src/data/sea_ray_sundancer38ft.js
@@ -1,7 +1,7 @@
-import img1 from '../assets/Sea ray Sundancer 38ft/1.jpg'
-import img2 from '../assets/Sea ray Sundancer 38ft/2.jpg'
-import img3 from '../assets/Sea ray Sundancer 38ft/3.jpg'
-import img4 from '../assets/Sea ray Sundancer 38ft/4.jpg'
+import img1 from '../assets/SeaRaySundancer38ft/1.jpg'
+import img2 from '../assets/SeaRaySundancer38ft/2.jpg'
+import img3 from '../assets/SeaRaySundancer38ft/3.jpg'
+import img4 from '../assets/SeaRaySundancer38ft/4.jpg'
 
 export const imagesSeaRaySundancer38ft = {
   img1,
